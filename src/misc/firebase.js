cba9649f0a0368123e1aa4172af45ba050cb38a4
Mehul9063 <mehulpotnuru1@gmail.com>
2021-06-13 10:32:31 +0530

    created private and public routes

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const config = {
+  apiKey: "AIzaSyDFcVAgEEBkhZrxM5tKVV1Y1usMJHZsIUg",
+  authDomain: "chat-web-app-1.firebaseapp.com",
+  databaseURL: "https://chat-web-app-1-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-1",
+  storageBucket: "chat-web-app-1.appspot.com",
+  messagingSenderId: "463492517947",
+  appId: "1:463492517947:web:051c03041174b862370c1e"
+}
+
+const app = firebase.initializeApp(config)
