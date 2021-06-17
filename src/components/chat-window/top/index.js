08b426d9f8319fa155e2d1588194e530a3298d4b
Mehul9063 <mehulpotnuru1@gmail.com>
2021-06-17 12:15:37 +0530

    create current room context with usecontextselector

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { memo } from 'react'
+import { useCurrentRoom } from '../../../context/currentRoom.context'
 
 const Top = () => {
+  
+  const name = useCurrentRoom(v => v.name)
+  
   return (
     <div>
-      top
+      {name}
     </div>
   )
 }
 
-export default Top
+export default memo(Top)
